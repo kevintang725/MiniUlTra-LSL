8a81584e8a01c98fecf363773cce257c5eafa3e7
Kevin Tang <kwkt725@gmail.com>
2023-07-31 12:03:55 -0500

    Update paradigm

--- Arduino/Legon_et_al_2014_v2/Legon_et_al_2014_v2.ino
+++ Arduino/Legon_et_al_2014_v2/Legon_et_al_2014_v2.ino
@@ -2,11 +2,12 @@
 int triggerPin_FUS = 12;
 int triggerPin_FES = 13;
 
-const byte LEDPin = 0;  // It's OK to use Pin 0 if you don't use Serial
-
 const unsigned long stimulation_period = 500; 
 const unsigned long rest_period = 500; 
 unsigned long StartOfInterval;
+const unsigned long  time;
+const unsigned long elapsed_epoch;
+const unsigned long epoch_rest = 1000;
 
 void setup()
 {
@@ -16,7 +17,9 @@ void setup()
   digitalWrite(triggerPin_FES, LOW);
   pinMode(triggerPin_FES, OUTPUT);
 
-  Serial.println("Stimulation beginning in...");
+  Serial.println("Stimulation beginning in 10 seconds...");
+  delay(10000)
+  Serial.println("Stimulation beginning in 3 seconds...");
   Serial.println("3");
   delay(1000);
   Serial.println("2");
@@ -28,20 +31,27 @@ void setup()
 
 void loop()
 {
+    /*
     // Check if 100ms has passed since start of FUS, if so begin FES (Median Nerve Stimulation)
     if ((millis() - time) > 100){
       digitalWrite(triggerPin_FES, HIGH);
       Serial.print("FES High");
     }
+    */
 
-    // Pulse Triggers for FUS
-    digitalWrite(triggerPin_FUS, HIGH);  
-    Serial.print('1');
-    Serial.print('\n');
+    elapsed_epoch = millis();
 
-    delay(rest_period);
+    if (elapsed_epoch < epoch_rest){
+      // Pulse Triggers for FUS
+      digitalWrite(triggerPin_FUS, HIGH);  
+      Serial.print('1');
+      Serial.print('\n');
 
-    digitalWrite(triggerPin_FUS, LOW);  
-    Serial.print('0');
-    Serial.print('\n');
+      delay(rest_period);
+
+      digitalWrite(triggerPin_FUS, LOW);  
+      Serial.print('0');
+      Serial.print('\n');
+
+    }
 }
