ee14c43f56772a28190cf022f7fed69c8a85a2ac
Kevin <kwkt725@gmail.com>
2023-08-01 14:43:00 -0500

    update EMG_Test

--- Code/BCI_Functions.py
+++ Code/BCI_Functions.py
@@ -18,9 +18,9 @@ def simple_moving_average(df, window_size=5):
     moving_average = df['column_name'].rolling(window_size).mean()
     return moving_average
 
-def weight_moving_average(df, window_size=5):
-    moving_average = df['column_name'].rolling(window_size).apply(lamda x: (weights*x).sum()/weights.sum())
-    return moving_average
+#def weight_moving_average(df, window_size=5):
+#    moving_average = df['column_name'].rolling(window_size).apply(lamda x: (weights*x).sum()/weights.sum())
+#    return moving_average
 
 def exponential_moving_average(df, window_size=5):
     moving_average= df['column_name'].ewm(span=window_size, adjust=False).mean()
